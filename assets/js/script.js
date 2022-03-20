810bccebc6caa73f9530625a45211aa6620d3cbc
Khyan Alvarez <khyanalvarez@gmail.com>
2022-03-19 23:40:05 -0400

    made it posible to add type to the tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
     
+    var taskNameInput = document.querySelector("input[name='task-name']").value; 
+    var taskTypeInput = document.querySelector("select[name='task-type']").value; 
+
+    //creat list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
-    tasksToDoEl.appendChild(listItemEl);
-};
 
+    //creates div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //gave div a class name
+    taskInfoEl.className = "task-info";
+    //adds html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
+};
+ 
 formEl.addEventListener("submit", createTaskHandler)
