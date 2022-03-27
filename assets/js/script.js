362d4f4b42b8579cd2f818e67eb845800397d76d
Khyan Alvarez <khyanalvarez@gmail.com>
2022-03-26 23:34:48 -0400

    troubleshooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,7 +88,6 @@ var createTaskAction = function(taskId) {
 
     //status array
     var statusChoices = ["To Do", "In Progress", "Completed"];
-     
     for (var i = 0; i < statusChoices.length; i++) {
         //create option element
         var statusOptionEl = document.createElement("option");
@@ -103,18 +102,45 @@ var createTaskAction = function(taskId) {
 }
 
 var taskButtonHandler = function(event) {
-    console.log(event.target);
+    //get element from event
+    var targetEl = event.target;
 
-    if (event.target.matches(".delete-btn")) {
-        var taskId = event.target.getAttribute("data-task-id");
+    //if delete button is clicked
+    if (targetEl.matches(".delete-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
         deleteTask(taskId);
+    }
+    //if edit button is clicked
+    else if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
     }
 };
 
 var deleteTask = function (taskId) {
      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']"); 
-    console.log(taskSelected);
+    taskSelected.remove()
 };
+
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+
+    //get task list item element 
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    console.log(taskName);
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    console.log(taskType);
+
+    //sending taskSelected name and type value back to the task-form
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+
+    //changes add task button's text content
+    document.querySelector('#save-task').textContent = "Save Changes";
+}
  
 formEl.addEventListener("submit", taskFormHandler);
 
